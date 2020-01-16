4560bab6c7ed0da8a3c46103fc4c52e2a2bbe36b
kdaviesnz <kdavies@gmail.com>
2020-01-17 07:24:51 +1300

    Update FunctionalGroupsTest.js

--- test/FunctionalGroupsTest.js
+++ test/FunctionalGroupsTest.js
@@ -16,6 +16,28 @@ const FunctionalGroupsTest = () => {
     g[2].should.be.equal('C1=CC2=C(C=C1)OCO2')
     g[3].should.be.equal('')
 
+    //!terminal alkene (safrole)
+const safrole = {
+       "CanonicalSMILES":'C=CCC1=CC2=C(C=C1)OCO2'
+   }
+
+   const safrolefg = FunctionalGroups(safrole).functionalGroups.terminal_alkene
+   // console.log(g)
+   safrolefg[0].should.be.equal('C=CCC1=CC2=C(C=C1)OCO2')
+   
+}
+
+
+// methyl ketone (methyl piperonal ketone)
+const mk = {
+       "CanonicalSMILES":'CC(=O)CC1=CC2=C(C=C1)OCO2'
+   }
+
+   const mkfg = FunctionalGroups(mk).functionalGroups.terminal_alkene
+   // console.log(g)
+   mkfg[0].should.be.equal('O')
+
+    
 }
 
 FunctionalGroupsTest()
