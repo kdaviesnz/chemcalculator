9c161b45dfdff06b483d0949f0acb457b710d257
Kevin Davies <kdavies@gmail.com>
2020-01-20 18:37:19 +1300

    ReactionsTest

--- test/ReactionsTest.js
+++ test/ReactionsTest.js
@@ -1,34 +1,44 @@
 require("dotenv").config()
 const should = require('should');
 const Reactions = require('../lib/CanonicalSmilesParserv2')
+const MoleculeLookup = require('../lib/MoleculeLookup')
 
 const ReactionsTest = () => {
 
     const verbose = true
-const methyl_piperonal_ketone = "CC(=O)CC1=CC2=C(C=C1)OCO2"
-    MoleculeLookup(db, methyl_piperonal_ketone, 'SMILES', true, "", (err)=>{
-        cinsole.log(err)
-    }).then(
-        (molecule_JSON_object) => {
-            console.log("[" + functional_group + "] " + molecule_JSON_object.IUPACName)
-            const parser = Reactions(
-                molecule_JSON_object,
-                db,
-                {},
-                (canonical_SMILES, substrate_JSON_object, reagents) => {
+    const methyl_piperonal_ketone = "CC(=O)CC1=CC2=C(C=C1)OCO2"
+
+    // Connect to mongo database
+    const MongoClient = require('mongodb').MongoClient
+    const S = require('string');
+
+    MongoClient.connect('mongodb+srv://' + process.env.MONGODBUSER + ':' + process.env.MONGODBPASSWORD + '!@cluster0-awqh6.mongodb.net', {useNewUrlParser: true},
+        function (err, client) {
+
+            console.log('Connecting to MONGO')
+            if (err) {
+                console.log(err)
+                process.exit()
+            }
+            const db = client.db('chemistry');
+
+            MoleculeLookup(db, methyl_piperonal_ketone, 'SMILES', true, "", (err) => {
+                console.log(err)
+            }).then(
+                (molecule_JSON_object) => {
+                    const parser = Reactions(
+                        molecule_JSON_object,
+                        db,
+                        {},
+                        (canonical_SMILES, substrate_JSON_object, reagents) => {
+                        }
+                    )
+                    parser.permanganateOxidationReverse(() => {
+
+                    })
                 }
             )
-parser.permanganateOxidationReverse((rule,
-                                        molecule_json_object,
-                                        substrate,
-                                        rule.reagents,
-                                        child_reaction_as_string,
-                                        "permanganateOxidationReverse")=> {
-}
-        }
-    )
-
-
+        })
 
 }
 
