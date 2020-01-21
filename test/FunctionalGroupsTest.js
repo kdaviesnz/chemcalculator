3fd83659c699bde9471066c29ddc9e05e65b3f11
Kevin Davies <kdavies@gmail.com>
2020-01-21 20:36:06 +1300

    alkene

--- test/FunctionalGroupsTest.js
+++ test/FunctionalGroupsTest.js
@@ -3,12 +3,30 @@ const FunctionalGroups = require('../lib/FunctionalGroups')
 
 const FunctionalGroupsTest = () => {
 
+    /*
+    [ [ 'C,CCC1=CC2=C(C=C1)OCO2C', 'CCC1=CC2=C(C=C1)OCO2' ] ]
+[ [ 'C', 'CCC1=CC2=C(C=C1)OCO2' ],
+  [ [ 'C,CCC1=CC2=C(C=C1)OCO2C', 'CCC1=CC2=C(C=C1)OCO2' ] ] ]
+
+     */
+    const safrole_alkene = {
+        "CanonicalSMILES":"C=CCC1=CC2=C(C=C1)OCO2"
+    }
+    const sfg = FunctionalGroups(safrole_alkene).functionalGroups.alkene
+    console.log(sfg)
+    sfg[0][0].should.be.equal('C')
+    sfg[0][1].should.be.equal('CCC1=CC2=C(C=C1)OCO2')
+    process.exit()
+
 // methyl ketone (methyl piperonal ketone)
     const mk = {
         "CanonicalSMILES":'CC(=O)CC1=CC2=C(C=C1)OCO2'
     }
 
     const mkfg = FunctionalGroups(mk).functionalGroups.methyl_ketone
+
+    console.log(mkfg)
+    process.exit()
     mkfg[2].should.be.equal('C')
 
     //!terminal alkene (safrole)
